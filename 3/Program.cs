2c82d506d8283ec7ff6a5553ad8dbb837db67d1c
Your Name <you@example.com>
2023-01-22 00:34:31 +0300

    добавила 3ю задачу

--- 3/Program.cs
+++ 3/Program.cs
@@ -1,2 +1,10 @@
 ﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
 
+Console.WriteLine ("Введите число для проверки на четность");
+int num = Convert.ToInt32 ( Console.ReadLine());
+int result = num % 2;
+    if ( result == 0)
+        Console.WriteLine ( "Число " + num +" является чётным");
+
+ else
+      Console.WriteLine ( "Число "+ num + " является нечётным");
