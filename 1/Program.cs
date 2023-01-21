21d2d095833f137b97e1bef0157cbd7b06f96b67
Your Name <you@example.com>
2023-01-22 00:21:52 +0300

    2 задачи

--- 1/Program.cs
+++ 1/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.WriteLine ("Введите два числа");
+
+int num1 = Convert.ToInt32 (Console.ReadLine());
+int num2 = Convert.ToInt32 (Console.ReadLine());
+
+if ( num1>num2)
+    Console.WriteLine ( "Число " + num1 + " больше числа " + num2);
+else
+    Console.WriteLine ( "Число " + num2 + " больше числа " + num1);
---
+++ 2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+
+  
+
+
+Console.WriteLine (" Введите 3 числа");
+
+int num1 = Convert.ToInt32 ( Console.ReadLine());
+int num2 = Convert.ToInt32 ( Console.ReadLine());
+int num3 = Convert.ToInt32 ( Console.ReadLine());
+
+  int result = num1;
+    if (num2 > result) result = num2;
+    if (num3 > result) result = num3;
+
+Console.WriteLine ("Максимальное число из трех: " + result);
